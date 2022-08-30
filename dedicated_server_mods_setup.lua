915f110a24444cdc376bff94ef7c9f44b28b84a5
Noppagorn <tongnop2540@gmail.com>
2022-08-31 01:04:21 +0700

    add enable mods script

--- dedicated_server_mods_setup.lua
+++ dedicated_server_mods_setup.lua
@@ -9,3 +9,30 @@
 	--The Workshop id can be found at the end of the url to the collection's Workshop page.
 	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
 	--ServerModCollectionSetup("379114180")
+
+--#Global Position
+ServerModSetup("378160973")
+
+--#Health Info
+ServerModSetup("375859599")
+
+--#Allied Wall
+ServerModSetup("2009905954")
+
+--#Gem Core
+ServerModSetup("1378549454")
+
+--#Display Attack Range
+ServerModSetup("2078243581")
+
+--#Display Food Values
+ServerModSetup("347079953")
+
+--#Wormhole Marks
+ServerModSetup("362175979")
+
+--#Beefalo Widget
+ServerModSetup("1852257480")
+
+--#One Click Action
+ServerModSetup("788268084")
